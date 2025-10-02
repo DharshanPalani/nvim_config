7f8b386dfb48be7f483b357dade882a730636cf1
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-10-02 18:35:28 +0530

    Commented out neo tree coz for some fuckin reason it kept comming back

--- lua/vim-keymaps.lua
+++ lua/vim-keymaps.lua
@@ -1,8 +1,8 @@
 local builtin = require("telescope.builtin")
---vim.keymap.set("n", "<Space><Space>", builtin.find_files, { desc = "Telescope find files" })
---vim.keymap.set("n", "<Space>f", builtin.live_grep, { desc = "Telescope live grep" })
-vim.keymap.set("n", "<C-e>", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
-vim.keymap.set("n", "<C-f>", ":Neotree focus<CR>",{desc = "Focus Neo-Tree"})
+vim.keymap.set("n", "<Space><Space>", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<Space>f", builtin.live_grep, { desc = "Telescope live grep" })
+--vim.keymap.set("n", "<C-e>", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
+--vim.keymap.set("n", "<C-f>", ":Neotree focus<CR>",{desc = "Focus Neo-Tree"})
 
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
